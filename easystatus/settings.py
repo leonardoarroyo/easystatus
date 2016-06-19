beee48440c5ca48b9a6cc83350a2a07f796c2feb
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-06-18 22:45:38 -0300

    Include dj_database_url

--- easystatus/settings.py
+++ easystatus/settings.py
@@ -74,11 +74,10 @@ WSGI_APPLICATION = 'easystatus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('mysql://root:@localhost:3306/easystatus')
 }
 
 
