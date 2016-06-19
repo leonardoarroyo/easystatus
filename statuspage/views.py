eb39423bcbc3294e4ccfa455d0d83a78eb770ae5
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-06-19 02:08:54 -0300

    Thin client to display basic info from the API working

--- statuspage/views.py
+++ statuspage/views.py
@@ -6,13 +6,26 @@ from rest_framework.test import APIClient
 import requests
 import json
 
-def index(request):
-  client = APIClient()
-  context = {}
+def api_request(route, kwargs=None):
+  """
+  Used to make internal request to easystatusapi
+  """
+  if not kwargs:
+    kwargs = {}
 
-  # Load status page
-  url = reverse('pages-detail', kwargs={'pk':1})
+  client = APIClient()
+  url = reverse(route, kwargs=kwargs)
   response = client.get(url)
-  context['status_page'] = json.loads(response.content.decode('utf8'))
+  return json.loads(response.content.decode('utf8'))
+
+def index(request):
+  """
+  Index view for status page
+  """
+  context = {}
+  context['status_page'] = api_request('pages-detail', kwargs={'pk': 1})
+  context['components'] = api_request('page-components-list', kwargs={'page_pk': 1})
+
+  print(context)
 
   return render(request, 'index.html', context)
