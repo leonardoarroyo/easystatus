bd351a0b0341482120f1520a04c33d5ae31534de
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-06-18 23:01:33 -0300

    Introduce StatusPage, Component and Test models

---
+++ easystatus-api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class StatusPage(models.Model):
+  name = models.CharField(max_length=100, null=False, blank=False)
+  description = models.CharField(max_length=1000, null=True, blank=True, default=None)
+
+class Component(models.Model):
+  status_page = models.ForeignKey(StatusPage)
+  name = models.CharField(max_length=100, null=False, blank=False)
+  description = models.CharField(max_length=1000, null=True, blank=True, default=None)
+
+class Test(models.Model):
+  name = models.CharField(max_length=100, null=False, blank=False)
+  description = models.CharField(max_length=1000, null=True, blank=True, default=None)
+  component = models.ForeignKey(Component)
