41482a64dbe3c9711856677c99a705b86ca8a0da
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-06-24 01:31:17 -0300

    Allow different status pages to be accessed by different urls

--- statuspage/urls.py
+++ statuspage/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from statuspage import views
 
 urlpatterns = [
-  url(r'^$', views.index),
+    url(r'^', include([
+      url(r'^$', views.index),
+      url(r'^(?P<status_page_id>\d+)', views.index)
+    ])),
 ]
--- statuspage/views.py
+++ statuspage/views.py
@@ -18,12 +18,23 @@ def api_request(route, kwargs=None):
   response = client.get(url)
   return json.loads(response.content.decode('utf8'))
 
-def index(request):
+def index(request, status_page_id=None):
   """
   Index view for status page
   """
+  if status_page_id:
+    pk = status_page_id
+  else:
+    pk = 1
+
+  # Get page details
   context = {}
-  context['status_page'] = api_request('pages-detail', kwargs={'pk': 1})
-  context['components'] = api_request('page-components-list', kwargs={'page_pk': 1})
+  context['status_page'] = api_request('pages-detail', kwargs={'pk': pk})
+
+  if 'detail' in context['status_page'] and context['status_page']['detail'] == 'Not found.':
+    return render(request, '404.html', status=404)
+
+  # Get components details
+  context['components'] = api_request('page-components-list', kwargs={'page_pk': pk})
 
   return render(request, 'index.html', context)
