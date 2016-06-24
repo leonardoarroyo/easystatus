6fc284ae86943479f1d28bf39cedb22ac4ab171e
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-06-23 21:42:49 -0300

    Basic admin for Components, StatusPages and Tests

--- easystatusapi/admin.py
+++ easystatusapi/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.core.urlresolvers import reverse
 
-# Register your models here.
+from easystatusapi.models import Component, StatusPage, Test
+
+class StatusPageAdmin(admin.ModelAdmin):
+  fields = ['id', 'name', 'description']
+  list_display = ['name', 'description']
+  readonly_fields = ['id']
+
+class ComponentAdmin(admin.ModelAdmin):
+  fields = ['id', 'name', 'description', 'status_page']
+  list_display = ['id', 'name', 'show_status_page']
+  readonly_fields = ['id']
+
+  def show_status_page(self, obj):
+    try:
+      page_name = obj.status_page.__str__()
+      pk = obj.status_page.id
+      url = reverse('admin:easystatusapi_statuspage_change', args=(pk,))
+      return format_html("<a href=\"{url}\">{name}</a>", name=page_name, url=url)
+    except:
+      pass
+    return "No status page"
+
+class TestAdmin(admin.ModelAdmin):
+  fields = ['id', 'name', 'description', 'component']
+  list_display = ['id', 'name', 'show_component']
+  readonly_fields = ['id']
+
+  def show_component(self, obj):
+    try:
+      component_name = obj.component.__str__()
+      pk = obj.component.id
+      url = reverse('admin:easystatusapi_component_change', args=(pk,))
+      return format_html("<a href=\"{url}\">{name}</a>", name=component_name, url=url)
+    except:
+      pass
+    return "No component"
+
+  show_component.short_description = "Component"
+
+admin.site.register(StatusPage, StatusPageAdmin)
+admin.site.register(Component, ComponentAdmin)
+admin.site.register(Test, TestAdmin)
--- easystatusapi/models/test.py
+++ easystatusapi/models/test.py
@@ -6,3 +6,6 @@ class Test(models.Model):
   name = models.CharField(max_length=100, null=False, blank=False)
   description = models.CharField(max_length=1000, null=True, blank=True, default=None)
   component = models.ForeignKey(Component)
+
+  def __str__(self):
+    return self.name
